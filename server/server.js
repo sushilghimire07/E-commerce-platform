c2c2248add5673edb41380a89cb41486b21529c9
Your New Name <sushilghimire1010@gmail.com>
2025-05-24 17:15:28 +0545

    First commit

---
+++ server/server.js
@@ -0,0 +1,40 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import cookieParser from 'cookie-parser'
+import cors  from 'cors'
+
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING).then(
+    ()=>{
+        console.log("Databse connected sucesfully...!!")
+    }
+).catch((error)=>{
+    console.log("Error while connecting to database..!! "+error)
+})
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.use(
+    cors({
+        origin:'http://localhost:5173',
+        methods:['GET','POST','DELETE','PUT'],
+        allowedHeaders:[
+            "Content-Type",
+            "Authorization",
+            "Cache-Control",
+            "Expires",
+            "pragma"
+        ],credentials:true
+    })
+)
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(PORT,()=>{
+ console.log(`Server is running on port : ${PORT}`)
+}) 
