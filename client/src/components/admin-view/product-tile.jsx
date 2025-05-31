492a6f9a4cb85a794f85ce09fe2bdd46096ea98f
Your New Name <sushilghimire1010@gmail.com>
2025-05-31 14:41:56 +0545

    Small bug fix

---
+++ client/src/components/admin-view/product-tile.jsx
@@ -0,0 +1,13 @@
+import { Card } from "../ui/card";
+
+function AdminProductTile(product) {
+    return ( 
+        <Card>
+            <div>
+                
+            </div>
+        </Card>
+     );
+}
+
+export default AdminProductTile;
