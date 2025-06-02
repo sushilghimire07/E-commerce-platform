f0115b57741941708b00c8b35e2b5f60f1534a86
Your New Name <sushilghimire1010@gmail.com>
2025-06-02 14:02:55 +0545

    Fixoing small delete bug

--- server/routes/admin/products-route.js
+++ server/routes/admin/products-route.js
@@ -7,7 +7,7 @@ const router = express.Router()
 router.post('/upload-image',upload.single('my_file'),handleImageUpload)
 router.post('/add',addProduct)
 router.put('/edit/:id',editProduct)
-router.delete('/delete',deleteProduct)
+router.delete('/delete/:id',deleteProduct)
 router.get('/get',fetchAllProducts)
 
 export default router
