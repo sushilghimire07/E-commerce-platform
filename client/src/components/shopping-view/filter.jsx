87be25dd7f1b11d5cc3eee3fcfc00d4c6d7f54e1
Your New Name <sushilghimire1010@gmail.com>
2025-06-03 23:17:23 +0545

    Making listing page

--- client/src/components/shopping-view/filter.jsx
+++ client/src/components/shopping-view/filter.jsx
@@ -1,11 +1,37 @@
+import { filterOptions } from "@/config";
+import { Fragment } from "react";
+import { Label } from "../ui/label";
+import { Checkbox } from "../ui/checkbox";
+
 function ProductFilter() {
     return ( 
         <div className="bg-background rounded-lg shadow-sm">
             <div className="p-4 border-b">
-                <h2 className="text-lg font-semibold">Filters</h2>
+                <h2 className="text-lg font-extrabold">Filters</h2>
+                
 
             </div>
 
+                <div className="p-4 space-y-4">
+                        {
+                            Object.keys(filterOptions).map(keyItem => 
+                            <Fragment>
+                                <div>
+                                    <h3 className="text-base font-bold">{keyItem}</h3>
+                                </div>
+                                <div className="grid gap-2 mt-2">
+                    {
+                        filterOptions[keyItem].map((option)=><Label className="flex  font-medium items-center gap-2 font-normal">
+                                <Checkbox/>
+                                {
+                                    option.label
+                                }
+                        </Label>)
+                    }
+                </div>
+                            </Fragment>)
+                        }
+                </div>
         </div>
      );
 }
