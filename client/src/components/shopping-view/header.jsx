e2720449656ab41073740272763aaebc53a4f9e7
Your New Name <sushilghimire1010@gmail.com>
2025-06-03 21:17:05 +0545

    Making shopping pgae

--- client/src/components/shopping-view/header.jsx
+++ client/src/components/shopping-view/header.jsx
@@ -1,11 +1,44 @@
-import { HousePlus,Menu } from "lucide-react";
+import { HousePlus,Menu, ShoppingCart } from "lucide-react";
 import { Link } from "react-router-dom";
 import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
 import { Button } from "../ui/button";
 import { useSelector } from "react-redux";
+import { shoppingViewHeaderMenuItems } from "@/config";
+import { DropdownMenu, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
+import { Avatar, AvatarFallback } from "../ui/avatar";
 
+
+    function MenuItems(){
+        return <nav className="flex flex-col mb-3 lg:mb-0 lg:items-center gap-6 lg:flex-row">
+                {
+                    shoppingViewHeaderMenuItems.map(MenuItems=>
+                         <Link className="text-sm font-medium" key={MenuItems.id} to={MenuItems.path}>
+                            {MenuItems.label}</Link>)
+                }
+        </nav>
+    }
+
+
+    function HeaderRightContent(){
+        return <div className="flex lg:items-center lg:flex-row flex-col gap-4">
+            <Button variant="outline" size="icon">
+                 <ShoppingCart className="w-6 h-6"/>
+                 <span className="sr-only">User cart</span>
+            </Button>
+           
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Avatar className="bg-black">
+                        <AvatarFallback  className="bg-black text-white font-extrabold">SG</AvatarFallback>
+                    </Avatar>
+                </DropdownMenuTrigger>
+            </DropdownMenu>
+        </div>
+    }
 
 function ShoppingHeader() {
+
+
 
    
     const {isAuthenticated}  = useSelector(state=>state.auth)
@@ -28,15 +61,17 @@ function ShoppingHeader() {
                 </Button>
             </SheetTrigger>
             <SheetContent side="left" className='w-full max-w-x6' >
-
+        <MenuItems/>
             </SheetContent>
          </Sheet>
 
         <div className="hidden lg:block">
-            
+            <MenuItems/>
         </div>
         {
-            isAuthenticated ? <div></div> :null
+            isAuthenticated ? <div>
+                <HeaderRightContent/>
+            </div> :null
         }
         </div>
         </header>
